1d76ec528719a4515ea3024e8878a0bd42536488
Gerard Winston Hamoy <winstonvahmoy89@gmail.com>
2024-12-16 17:04:36 +0800

    Initial Commit

---
+++ iOSExamSampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOSExamSampleApp
+//
+//  Created by Gerard Winston Hamoy on 12/16/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iOSExamSampleAppTests/iOSExamSampleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  iOSExamSampleAppTests.swift
+//  iOSExamSampleAppTests
+//
+//  Created by Gerard Winston Hamoy on 12/16/24.
+//
+
+import Testing
+@testable import iOSExamSampleApp
+
+struct iOSExamSampleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
